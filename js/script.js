a5373d9e8fedff0833a2e227090b4a537c747c47
anait07 <anaitullah198@gmail.com>
2024-10-03 13:25:44 +0800

    Sort by categories

--- js/script.js
+++ js/script.js
@@ -12,13 +12,25 @@ const getVideos = () => {
         .then(data => showVideos(data.videos));
 }
 
+// const loadCategories = (id) => {
+//     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+//         .then(res => res.json())
+//         .then(data => showVideos(data.category));
+// }
+
+const loadCategories = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then(res => res.json())
+        .then(data => showVideos(data.category));
+}
+
 const showCategories = (data) => {
     const categoryContainer = document.getElementById('catergories');
     data.forEach(category => {
         const div = document.createElement('div');
         categoryContainer.classList = 'flex justify-center gap-4'
         div.innerHTML = `
-        <button class ="btn">${category.category}</button>
+        <button onclick="loadCategories(${category.category_id})" class ="btn">${category.category}</button>
         `
         categoryContainer.appendChild(div);
 
@@ -28,17 +40,31 @@ const showCategories = (data) => {
 
 const showVideos = (videos) => {
     const cardContainer = document.getElementById('car-container');
+    cardContainer.innerHTML = "";
+    // console.log(videos)
+    if(!videos.length) {
+        // cardContainer.classList.remove('grid')
+        cardContainer.innerHTML = `
+        <div class="h-[400px] w-full col-span-4  text-center">
+            <img class=" w-[250px] my-12 mx-auto" src="../assets/Icon.png" />
+            <div class="text-center mb-6 text-3xl font-bold">
+                <h2>Oops!! Sorry, There is no</h2>
+                <h2>content here<h2/>
+            <div/>
+        </div>
+        `;
+        return
+    }
     videos.forEach(video => {
-        console.log(video)
+        // console.log(video)
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card card-compact bg-base-100 pb-4">
                 <figure class="h-[250px] relative">
                     <img class="h-full w-full object-cover" src=${video.thumbnail} alt="thumbnail" />
                 </figure>
-                ${
-                    video.others.posted_date ? `<p class="absolute bg-black p-2 rounded-md text-white top-4 right-5">${video.others.posted_date}</p>` : ""
-                }
+                ${video.others.posted_date ? `<p class="absolute bg-black p-2 rounded-md text-white top-4 right-5">${video.others.posted_date}</p>` : ""
+            }
                 
                 <div class="ml-3 mt-4">
                     <h2 class="card-title">${video.title}</h2>
